ad7fc17594c8690aa409c65076c9317b2a4b8fb2
Bart Kang <bart.kang.0719@gmail.com>
2024-08-09 11:08:34 -0700

    implement room

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import "./style.css";
 
 const socket = io("http://localhost:8000");
 
+let room = "1";
+
+socket.emit("join-room", room);
+
 $("#app").html(`
   <div>
-    <h1>Chatroom</h1>
+    <div class="rooms">
+      <button class="room-1">Join Room 1</button>
+      <button class="room-2">Join Room 2</button>
+    </div>
+
+    <h1>Chatroom <span>${room}</span></h1>
 
     <ul>
       <li>demo message</li>
@@ -21,6 +30,18 @@ $("#app").html(`
 const formElement = $("form");
 const inputElement = $("input");
 const ulElement = $("ul");
+const rooms = $(".rooms");
+const span = $("span");
+
+rooms.on("click", (event: JQuery.ClickEvent) => {
+  const roomId = (event.target as HTMLButtonElement).className.split("-")[1];
+
+  room = roomId;
+  span.text(room);
+
+  socket.emit("join-room", room);
+});
+
 formElement.on("submit", (event: JQuery.SubmitEvent) => {
   event.preventDefault();
 
@@ -29,7 +50,7 @@ formElement.on("submit", (event: JQuery.SubmitEvent) => {
   const listItem = $(`<li>${inputValue}</li>`);
   ulElement.append(listItem);
 
-  socket.emit("message", inputValue);
+  socket.emit("message", inputValue, room);
 });
 
 socket.on("message", (message) => {
